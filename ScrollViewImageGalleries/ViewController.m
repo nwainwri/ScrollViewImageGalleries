82fbaeecd2ac9b3ffa2e3d985af841a86390111f
Nathan Wainwright <nathanwainwright@gmail.com>
2018-08-13 18:54:18 -0700

    programmatically added three UIImageViews.

--- ScrollViewImageGalleries/ViewController.m
+++ ScrollViewImageGalleries/ViewController.m
@@ -9,15 +9,148 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *imageScrollViewArea;
 
 @end
 
-@implementation ViewController
+@implementation ViewController 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.imageScrollViewArea.delegate = self;
+    
+    // CRREATE THREE UI VIEWS
+    
+    // add them as subview to sroll view...?
+    
+    //  A red UIView at (20,20) x, y coordinates and with width 100 and height 100
+    UIImageView *firstImage = [[UIImageView alloc] initWithFrame:CGRectZero];
+    firstImage.translatesAutoresizingMaskIntoConstraints = NO;
+    firstImage.backgroundColor = [UIColor orangeColor];
+    [self.imageScrollViewArea addSubview:firstImage];
+    
+    //  HEIGHT
+    [NSLayoutConstraint constraintWithItem:firstImage
+                                 attribute:NSLayoutAttributeHeight
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:nil
+                                 attribute:NSLayoutAttributeNotAnAttribute
+                                multiplier:1
+                                  constant:100].active = YES;
+    
+    //    WIDTH
+    [NSLayoutConstraint constraintWithItem:firstImage
+                                 attribute:NSLayoutAttributeWidth
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:nil
+                                 attribute:NSLayoutAttributeNotAnAttribute
+                                multiplier:1
+                                  constant:100].active = YES;
+    
+    //    RIGHT EDGE
+    [NSLayoutConstraint constraintWithItem:firstImage
+                                 attribute:NSLayoutAttributeLeft
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.imageScrollViewArea
+                                 attribute:NSLayoutAttributeLeft
+                                multiplier:1
+                                  constant:20].active = YES;
+    
+    //    TOP EDGE
+    [NSLayoutConstraint constraintWithItem:firstImage
+                                 attribute:NSLayoutAttributeTop
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.imageScrollViewArea
+                                 attribute:NSLayoutAttributeTop
+                                multiplier:1
+                                  constant:20].active = YES;
+    
+    UIImageView *secondImage = [[UIImageView alloc] initWithFrame:CGRectZero];
+    secondImage.translatesAutoresizingMaskIntoConstraints = NO;
+    secondImage.backgroundColor = [UIColor orangeColor];
+    [self.imageScrollViewArea addSubview:secondImage];
+    
+    //  HEIGHT
+    [NSLayoutConstraint constraintWithItem:secondImage
+                                 attribute:NSLayoutAttributeHeight
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:nil
+                                 attribute:NSLayoutAttributeNotAnAttribute
+                                multiplier:1
+                                  constant:100].active = YES;
+    
+    //    WIDTH
+    [NSLayoutConstraint constraintWithItem:secondImage
+                                 attribute:NSLayoutAttributeWidth
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:nil
+                                 attribute:NSLayoutAttributeNotAnAttribute
+                                multiplier:1
+                                  constant:100].active = YES;
+    
+    //    RIGHT EDGE
+    [NSLayoutConstraint constraintWithItem:secondImage
+                                 attribute:NSLayoutAttributeLeft
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.imageScrollViewArea
+                                 attribute:NSLayoutAttributeLeft
+                                multiplier:1
+                                  constant:20].active = YES;
+    
+    //    TOP EDGE
+    [NSLayoutConstraint constraintWithItem:secondImage
+                                 attribute:NSLayoutAttributeTop
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.imageScrollViewArea
+                                 attribute:NSLayoutAttributeTop
+                                multiplier:1
+                                  constant:20].active = YES;
+    
+    UIImageView *thirdImage = [[UIImageView alloc] initWithFrame:CGRectZero];
+    thirdImage.translatesAutoresizingMaskIntoConstraints = NO;
+    thirdImage.backgroundColor = [UIColor orangeColor];
+    [self.imageScrollViewArea addSubview:thirdImage];
+    
+    //  HEIGHT
+    [NSLayoutConstraint constraintWithItem:thirdImage
+                                 attribute:NSLayoutAttributeHeight
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:nil
+                                 attribute:NSLayoutAttributeNotAnAttribute
+                                multiplier:1
+                                  constant:100].active = YES;
+    
+    //    WIDTH
+    [NSLayoutConstraint constraintWithItem:thirdImage
+                                 attribute:NSLayoutAttributeWidth
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:nil
+                                 attribute:NSLayoutAttributeNotAnAttribute
+                                multiplier:1
+                                  constant:100].active = YES;
+    
+    //    RIGHT EDGE
+    [NSLayoutConstraint constraintWithItem:thirdImage
+                                 attribute:NSLayoutAttributeLeft
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.imageScrollViewArea
+                                 attribute:NSLayoutAttributeLeft
+                                multiplier:1
+                                  constant:20].active = YES;
+    
+    //    TOP EDGE
+    [NSLayoutConstraint constraintWithItem:thirdImage
+                                 attribute:NSLayoutAttributeTop
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.imageScrollViewArea
+                                 attribute:NSLayoutAttributeTop
+                                multiplier:1
+                                  constant:20].active = YES;
+    
+//    n your view controller's -viewDidLoad method, create three UIImageView and place them inside the UIScrollView next to each other. Now would be a good time to consider your layout strategy: are you going to use explicit constraints, or set the frame and rely on translatesAutoresizingMaskIntoConstraints?
     // Do any additional setup after loading the view, typically from a nib.
 }
+
 
 
 - (void)didReceiveMemoryWarning {
